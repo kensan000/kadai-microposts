2df631b242c2f8a616c10d0d63e5c111cad77ea9
ken.nakae <ken.techacademy@gmail.com>
2021-08-02 11:34:38 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
+
+Route::get('/', 'MicropostsController@index');
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,9 +23,8 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 */
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
