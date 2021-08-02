a21ec5d46e3d717c2d5bbfb1abfdd9109a4a0e91
ken.nakae <ken.techacademy@gmail.com>
2021-08-02 01:29:21 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
